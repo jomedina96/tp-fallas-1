80af15d76c28c4c0c125b6532126d3293910a5b0
jomedina96 <jcmedina@fi.uba.ar>
2022-01-18 21:07:32 -0300

    now you can get partial results

--- app.py
+++ app.py
@@ -8,47 +8,74 @@ import os
 import json
 import random
 
-app = Flask(__name__, template_folder='')
+app = Flask(__name__, template_folder="")
 openings = {}
-with open('static/aperturas.json') as json_data:
+with open("static/aperturas.json") as json_data:
     openings = json.load(json_data)
 
 
-@app.route('/favicon.ico')
+@app.route("/favicon.ico")
 def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
+    return send_from_directory(
+        os.path.join(app.root_path, "static"),
+        "favicon.ico",
+        mimetype="image/vnd.microsoft.icon",
+    )
 
 
 @app.route("/")
 def home():
-    return render_template('fallas.html')
+    return render_template("fallas.html")
 
 
-@app.route("/result", methods=['POST'])
+@app.route("/result", methods=["POST"])
 def result():
     body = request.get_json()
-    user_opening = UserOpening(color=body["color"],
-                               position=body["position"],
-                               difficulty=int(body["difficulty"]),
-                               style=body["style"],
-                               minimum_time=int(body["minimum_time"]),
-                               objective=body["objective"])
+    user_opening = None
+    if body["difficulty"] != "" and body["minimum_time"] != "":
+        user_opening = UserOpening(
+            color=body["color"],
+            position=body["position"],
+            difficulty=int(body["difficulty"]),
+            style=body["style"],
+            minimum_time=int(body["minimum_time"]),
+            objective=body["objective"],
+        )
+    elif body["difficulty"] != "":
+        user_opening = UserOpening(
+            color=body["color"],
+            position=body["position"],
+            difficulty=int(body["difficulty"]),
+            style=body["style"],
+            minimum_time=None,
+            objective=body["objective"],
+        )
+    else:
+        user_opening = UserOpening(
+            color=body["color"],
+            position=body["position"],
+            difficulty=None,
+            style=body["style"],
+            minimum_time=None,
+            objective=body["objective"],
+        )
 
     opening_selected = None
     max_hit = 0
     hitted_map = {}
 
     for child in Opening.__subclasses__():
-        if (child == UserOpening):
+        if child == UserOpening:
             continue
 
         count_field = child().compareFields(user_opening)
-        if (max_hit <= count_field):
+        if max_hit <= count_field:
             max_hit = count_field
             hitted_map[max_hit] = child()
 
     opening_selected = None
-    if (max_hit >= 4):
+    print(hitted_map)
+    if max_hit >= 4:
         opening_selected = hitted_map[max_hit]
     opening_data = {"name": ""}
 
@@ -61,7 +88,7 @@ def result():
     return jsonify(opening_data), 200
 
 
-@app.route("/result2", methods=['POST'])
+@app.route("/result2", methods=["POST"])
 def result2():
     body = request.get_json()
-    answers = body['answers']
+    answers = body["answers"]
--- openings.py
+++ openings.py
@@ -1,5 +1,14 @@
 class Opening(object):
-    def __init__(self, name=None, color=None, position=None, difficulty=None, style=None, minimum_time=None, objective=None):
+    def __init__(
+        self,
+        name=None,
+        color=None,
+        position=None,
+        difficulty=None,
+        style=None,
+        minimum_time=None,
+        objective=None,
+    ):
         self._name = name
         self._color = color
         self._position = position
@@ -9,10 +18,14 @@ class Opening(object):
         self._objective = objective
 
     def __eq__(self, other):
-        return self.color == other.color and self.position == other.position and \
-            self.difficulty == other.difficulty and self.style == other.style and \
-            self.minimum_time == other.minimum_time and \
-            self.objective == other.objective
+        return (
+            self.color == other.color
+            and self.position == other.position
+            and self.difficulty == other.difficulty
+            and self.style == other.style
+            and self.minimum_time == other.minimum_time
+            and self.objective == other.objective
+        )
 
     def compareFields(self, other):
         count = 0
@@ -21,19 +34,27 @@ class Opening(object):
             count += 1
         if self.position == other.position:
             count += 1
-        if self.difficulty == other.difficulty:
+        if other.difficulty is None or self.difficulty == other.difficulty:
             count += 1
-        if self.style == other.style:
+        if other.style == "" or self.style == other.style:
             count += 1
-        if self.minimum_time == other.minimum_time:
+        if other.minimum_time is None or self.minimum_time == other.minimum_time:
             count += 1
-        if self.objective == other.objective:
+        if other.objective == "" or self.objective == other.objective:
             count += 1
 
         return count
 
     def __str__(self):
-        return self._name
+        return "name: {}, color: {}, position: {}, difficulty: {}, style {}, mininum time: {}, objective".format(
+            self._name,
+            self.color,
+            self.position,
+            self.difficulty,
+            self.style,
+            self.minimum_time,
+            self.objective,
+        )
 
     @property
     def name(self):
@@ -94,95 +115,207 @@ class Opening(object):
 
 class ItalianOpening(Opening):
     def __init__(self):
-        super(ItalianOpening, self).__init__(name="Apertura Italiana", color="white",
-                                             position="open", difficulty=1, style="dinamic", minimum_time=1, objective="win")
+        super(ItalianOpening, self).__init__(
+            name="Apertura Italiana",
+            color="white",
+            position="open",
+            difficulty=1,
+            style="dinamic",
+            minimum_time=1,
+            objective="win",
+        )
 
 
 class RuyLopezOpening(Opening):
     def __init__(self):
-        super(RuyLopezOpening, self).__init__(name="Ruy Lopez", color="white",
-                                              position="open", difficulty=3, style="dinamic", minimum_time=3, objective="win")
+        super(RuyLopezOpening, self).__init__(
+            name="Ruy Lopez",
+            color="white",
+            position="open",
+            difficulty=3,
+            style="dinamic",
+            minimum_time=3,
+            objective="win",
+        )
 
 
 class ChigorinOpening(Opening):
     def __init__(self):
-        super(ChigorinOpening, self).__init__(name="Defensa Francesa: variacion Chingorin", color="white",
-                                              position="semi-open", difficulty=3, style="dinamic", minimum_time=1, objective="stalemate")
+        super(ChigorinOpening, self).__init__(
+            name="Defensa Francesa: variacion Chingorin",
+            color="white",
+            position="semi-open",
+            difficulty=3,
+            style="dinamic",
+            minimum_time=1,
+            objective="stalemate",
+        )
 
 
 class LondonOpening(Opening):
     def __init__(self):
-        super(LondonOpening, self).__init__(name="Sistema London", color="white", position="closed",
-                                            difficulty=1, style="positional", minimum_time=2, objective="stalemate")
+        super(LondonOpening, self).__init__(
+            name="Sistema London",
+            color="white",
+            position="closed",
+            difficulty=1,
+            style="positional",
+            minimum_time=2,
+            objective="stalemate",
+        )
 
 
 class KingsIndianAttackOpening(Opening):
     def __init__(self):
-        super(KingsIndianAttackOpening, self).__init__(name="Ataque Kings Indian", color="white",
-                                                       position="closed", difficulty=4, style="positional", minimum_time=3, objective="win")
+        super(KingsIndianAttackOpening, self).__init__(
+            name="Ataque Kings Indian",
+            color="white",
+            position="closed",
+            difficulty=4,
+            style="positional",
+            minimum_time=3,
+            objective="win",
+        )
 
 
 class KingsIndianDefenseOpening(Opening):
     def __init__(self):
-        super(KingsIndianDefenseOpening, self).__init__(name="Defensa Kings Indian", color="black",
-                                                        position="closed", difficulty=2, style="positional", minimum_time=2, objective="stalemate")
+        super(KingsIndianDefenseOpening, self).__init__(
+            name="Defensa Kings Indian",
+            color="black",
+            position="closed",
+            difficulty=2,
+            style="positional",
+            minimum_time=2,
+            objective="stalemate",
+        )
 
 
 class CaroKannOpening(Opening):
     def __init__(self):
-        super(CaroKannOpening, self).__init__(name="Caro Kann", color="black",
-                                              position="semi-open", difficulty=3, style="positional", minimum_time=3, objective="win")
+        super(CaroKannOpening, self).__init__(
+            name="Caro Kann",
+            color="black",
+            position="semi-open",
+            difficulty=3,
+            style="positional",
+            minimum_time=3,
+            objective="win",
+        )
 
 
 class HungarianDefenseOpening(Opening):
     def __init__(self):
-        super(HungarianDefenseOpening, self).__init__(name="Defensa Hungara", color="black",
-                                                      position="closed", difficulty=5, style="dinamic", minimum_time=3, objective="stalemate")
+        super(HungarianDefenseOpening, self).__init__(
+            name="Defensa Hungara",
+            color="black",
+            position="closed",
+            difficulty=5,
+            style="dinamic",
+            minimum_time=3,
+            objective="stalemate",
+        )
 
 
 class GrecoOpening(Opening):
     def __init__(self):
-        super(GrecoOpening, self).__init__(name="Defensa Greco", color="black",
-                                           position="open", difficulty=2, style="dinamic", minimum_time=1, objective="win")
+        super(GrecoOpening, self).__init__(
+            name="Defensa Greco",
+            color="black",
+            position="open",
+            difficulty=2,
+            style="dinamic",
+            minimum_time=1,
+            objective="win",
+        )
 
 
 class ScandinavianOpening(Opening):
     def __init__(self):
-        super(ScandinavianOpening, self).__init__(name="Defensa Escandinava", color="black",
-                                                  position="semi-open", difficulty=1, style="dinamic", minimum_time=1, objective="win")
+        super(ScandinavianOpening, self).__init__(
+            name="Defensa Escandinava",
+            color="black",
+            position="semi-open",
+            difficulty=1,
+            style="dinamic",
+            minimum_time=1,
+            objective="win",
+        )
 
 
 class StonewallAttackOpening(Opening):
     def __init__(self):
-        super(StonewallAttackOpening, self).__init__(name="Ataque Stonewall ", color="white",
-                                                     position="closed", difficulty=2, style="positional", minimum_time=2, objective="stalemate")
+        super(StonewallAttackOpening, self).__init__(
+            name="Ataque Stonewall ",
+            color="white",
+            position="closed",
+            difficulty=2,
+            style="positional",
+            minimum_time=2,
+            objective="stalemate",
+        )
 
 
 class GrunfeldDefenceOpening(Opening):
     def __init__(self):
-        super(GrunfeldDefenceOpening, self).__init__(name="Defensa Grunfeld", color="black",
-                                                     position="closed", difficulty=5, style="positional", minimum_time=3, objective="win")
+        super(GrunfeldDefenceOpening, self).__init__(
+            name="Defensa Grunfeld",
+            color="black",
+            position="closed",
+            difficulty=5,
+            style="positional",
+            minimum_time=3,
+            objective="win",
+        )
 
 
 class AlekhineDefenceOpening(Opening):
     def __init__(self):
-        super(AlekhineDefenceOpening, self).__init__(name="Defensa Alekhine", color="black",
-                                                     position="open", difficulty=4, style="positional", minimum_time=3, objective="win")
+        super(AlekhineDefenceOpening, self).__init__(
+            name="Defensa Alekhine",
+            color="black",
+            position="open",
+            difficulty=4,
+            style="positional",
+            minimum_time=3,
+            objective="win",
+        )
 
 
 class RetiOpening(Opening):
     def __init__(self):
-        super(RetiOpening, self).__init__(name="Apertura Reti", color="white",
-                                          position="semi-open", difficulty=4, style="positional", minimum_time=2, objective="win")
+        super(RetiOpening, self).__init__(
+            name="Apertura Reti",
+            color="white",
+            position="semi-open",
+            difficulty=4,
+            style="positional",
+            minimum_time=2,
+            objective="win",
+        )
 
 
 class TorreAttackOpening(Opening):
     def __init__(self):
-        super(TorreAttackOpening, self).__init__(name="Ataque Torre", color="white",
-                                                 position="open", difficulty=2, style="dinamic", minimum_time=2, objective="win")
+        super(TorreAttackOpening, self).__init__(
+            name="Ataque Torre",
+            color="white",
+            position="open",
+            difficulty=2,
+            style="dinamic",
+            minimum_time=2,
+            objective="win",
+        )
 
 
 class UserOpening(Opening):
     def __init__(self, color, position, difficulty, style, minimum_time, objective):
-        super(UserOpening, self).__init__(name="User opening", color=color, position=position,
-                                          difficulty=difficulty, style=style, minimum_time=minimum_time, objective=objective)
+        super(UserOpening, self).__init__(
+            name="User opening",
+            color=color,
+            position=position,
+            difficulty=difficulty,
+            style=style,
+            minimum_time=minimum_time,
+            objective=objective,
+        )
